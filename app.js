fe9f0f3467c4837b27c3194a7595f5a0eb118027
Deep <deepvijay60@gmail.com>
2023-03-27 15:45:07 -0400

    2

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 //jshint esversion:6
 
 const express = require("express");
+const ejs = require('ejs');
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const path = require('path');
 const _ = require("lodash");
 
 const app = express();
 
 app.set('view engine', 'ejs');
-
+app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
